d6339f5e17946610a7891f29ed000a9da96bace1
Aleksej <skigfa@gmail.com>
2023-02-09 21:38:07 +0300

    Исправляем SQL-запросы в методах getByName & add, фильтруем входные значения и т.п.

--- 2.php
+++ 2.php
@@ -58,9 +58,12 @@ class User
      * @param string $name
      * @return array || boolean
      */
-    public static function user(string $name): array
+    public static function getByName(string $name): array
     {
-        $stmt = self::getInstance()->prepare("SELECT id, name, lastName, from, age, settings FROM Users WHERE name = {$name}");
+        //1) исправляем ошибки в SQL-запросе, from - зарезервированное слово, так что оборачиваем в ``:
+        $stmt = self::getInstance()->prepare("SELECT `id`, `name`, `lastName`, `from`, `age`, `settings` FROM Users WHERE name = :name");
+        //2) вот тут точно не вспомню, но суть в том, что перед отправой запроса входные значения надо обезвредить:
+        $stmt->bindValue(':name', (string) $name, self::getInstance()::PARAM_STR);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
@@ -74,9 +77,14 @@ class User
      */
     public static function add(string $name, string $lastName, int $age): string
     {
-        $sth = self::getInstance()->prepare("INSERT INTO Users (name, lastName, age) VALUES (:name, :age, :lastName)");
-        $sth->execute([':name' => $name, ':age' => $age, ':lastName' => $lastName]);
-
+        //1) если раннее PDO был назван, как $stmt, то и в остальных методах он также должен быть назван, как $stmt, а не как вздумается
+        //2) далее, также обезвреживаем входные данные перед отправкой запроса, как и в методе getByName()
+        //3) в SQL-запросе перепутаны поля, т.е. в поле lastName войдет age, исправим, правильный порядок Имя, Фамилия, Возраст
+        $stmt = self::getInstance()->prepare("INSERT INTO Users (name, lastName, age) VALUES (:name, :lastName, :age)");
+        $stmt->bindValue(':name', (string) $name, self::getInstance()::PARAM_STR);
+        $stmt->bindValue(':lastName', (string) $lastName, self::getInstance()::PARAM_STR);
+        $stmt->bindValue(':age', (int) $age, self::getInstance()::PARAM_INT);
+        $stmt->execute();
         return self::getInstance()->lastInsertId();
     }
 }
